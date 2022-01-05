3233019fe136a78fe5af8ec21173599126d7d90b
Agata Pietralik <agata.pietralik@student.wat.edu.pl>
2022-01-05 17:03:45 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="main">
+        <h2 class="header">Whose repositories would you like to see?</h2>
+        <form>
+            <label for="user-name">github @: </label>
+            <input v-model="name" type="text" id="user-name" name="user-name">
+            <button class="accept-button" @click="userName = name">OK</button>
+        </form>
+
+        <h2 class="header">The most popular repositories of user: {{userName}}</h2> 
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import UserInput from './components/UserInput';
+import {ref, watch} from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+
+  setup() {
+    const name = ref(null)
+    const userName = ref(null)
+
+    watch(()=> {
+      fetch('https://api.github.com/users/octocat/repos')
+      .then((response)=> response.json)
+      .then((data)=> {
+        console.log("data", data);
+      });
+    });
+
+    return {
+      name,
+      userName,
+    }
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: whitesmoke;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.main {
+  max-width: 60vw;
+  margin: auto;
+  background-color: white;
+  padding: 1rem;
+  border: 1px solid thistle;
+  border-radius: 10px;
+}
+
+.accept-button {
+  background-color: thistle;
+  margin: 0.5rem 2rem;
+  padding: 0.5rem 2rem;
+  border: 1px solid rgb(107, 85, 107);
+  border-radius: 5px;
+}
+
 </style>
---
+++ src/components/UserInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Whose reporitories would you like to see?</h1>
+        <form>
+            <label for="user-name">github @:</label>
+            <input type="text" id="user-name" name="user-name">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
