2ebfa8b2794acb51982b11f6983d3b35442f9c68
Agata Pietralik <agata.pietralik@student.wat.edu.pl>
2022-01-07 22:44:08 +0100

    Composition API works, TODO: sorting

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,76 @@
 <template>
-      <div class="main">
-        <h2 class="header">Whose repositories would you like to see?</h2>
-        <form>
-            <label for="user-name">github @: </label>
-            <input v-model="name" type="text" id="user-name" name="user-name">
-            <button class="accept-button" @click="userName = name">OK</button>
-        </form>
+  <div class="main">
+    <h2 class="header1">Whose repositories would you like to see?</h2>
+    
+    <label for="user-name">github @: </label>
+    <input
+      v-model="name"
+      id="user-name"
+      name="user-name"
+      placeholder="github user name here"
+    />
+    <button class="accept-button" @click="userName = name; isActive = false">OK</button>
+     
 
-        <h2 class="header">The most popular repositories of user: {{userName}}</h2> 
-    </div>
+    <h4 :class="{inactive:isActive}">
+      The most popular repositories of {{ userName }}:
+    </h4>
+
+
+    
+
+      <ListElement 
+        v-for="lib in data" 
+        :key="lib.name" 
+        :repository-name="lib.name" 
+        :description="lib.description"
+        :stars="lib.stargazers_count">
+      </ListElement>
+
+     <!-- <li v-for="lib in data" :key="lib.name" >{{lib.name}}</li> -->
+    
+
+  </div>
 </template>
 
 <script>
-//import UserInput from './components/UserInput';
-import {ref, watch} from "vue";
+import ListElement from './components/ListElement';
+import { ref, reactive, toRefs, watchEffect } from "vue";
+//import ListElementVue from './components/ListElement.vue';
 export default {
   name: "App",
-  components: {},
+
+  components: {
+    ListElement,
+  },
 
   setup() {
-    const name = ref(null)
-    const userName = ref(null)
+    const name = ref(null);
+    const userName = ref(null);
+    const state = reactive({ data: [] });
+   // const ListedElement = ListElement();
 
-    watch(()=> {
-      fetch('https://api.github.com/users/octocat/repos')
-      .then((response)=> response.json)
-      .then((data)=> {
-        console.log("data", data);
-      });
+    watchEffect(() => {
+      if (userName.value){
+        fetch(`https://api.github.com/users/${userName.value}/repos`)
+          .then((response) => response.json())
+          .then((data) => {
+            state.data = data;
+            console.log("data", data);
+            name.value = "";
+          });
+        //isClicked = true;
+      }
     });
 
     return {
+      isActive: true,
       name,
       userName,
-    }
-  }
+      ListElement,
+      ...toRefs(state),
+    };
+  },
 };
 </script>
 
@@ -46,26 +82,48 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
 }
 
 .main {
-  max-width: 60vw;
+  width: fit-content;
   margin: auto;
   background-color: white;
-  padding: 1rem;
-  border: 1px solid thistle;
+  padding: 0.6rem 1.5rem;
+  border: 1px solid #2c3e50;
   border-radius: 10px;
+  transition: 3s;
+  transition-timing-function: ease;
+}
+
+.inactive {
+  color:rgb(226, 226, 226);
+  transition: 2s;
 }
 
 .accept-button {
-  background-color: thistle;
+  color: white;
+  font-weight: 600;
+  background: rgb(34,158,94);
+background: radial-gradient(circle, #31a851 28%, rgba(31,149,52,1) 78%);
+  background-size: 100% auto;
   margin: 0.5rem 2rem;
-  padding: 0.5rem 2rem;
-  border: 1px solid rgb(107, 85, 107);
+  padding: 0.25rem 1.5rem;
+  border: 1px solid #2c3e50;
   border-radius: 5px;
+  transition: 0.3s all ease;
+  cursor: pointer;
+  box-shadow: 0 2px 10px rgb(217, 236, 220);
 }
+.accept-button:hover {
+  background-size: 200%;
+  background-position: center;
+  box-shadow: 0 6px 16px rgb(202, 226, 205);
+  transform: translate(0px, -1px);
+  letter-spacing: 0.05rem;
+}
+
+
 
 </style>
---
+++ src/components/ListElement.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <hr>
+        <p class="repo-name">
+            {{ repositoryName }}
+            &ensp;
+            <svg width="15" height="13" viewBox="0 -1 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.5 1L9.33389 4.97587L13.6819 5.49139L10.4673 8.46413L11.3206 12.7586L7.5 10.62L3.6794 12.7586L4.5327 8.46413L1.31813 5.49139L5.66611 4.97587L7.5 1Z" stroke="#666666" stroke-width="1.2" stroke-linejoin="round"/>
+            </svg>
+
+            <span class="star"> {{ stars }}</span>
+        </p>
+        <p class="desc">{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'repositoryName',
+        'description',
+        'stars'
+    ],
+}
+</script>
+
+<style>
+hr {
+    border-top: 0.6px solid #cccccc;
+}
+.repo-name {
+    font-weight: 600;
+    font-size: 1rem;
+    color: #0862CC;
+}
+
+.svg {
+    vertical-align: middle;;
+}
+
+.star {
+    vertical-align: middle;
+    font-size: 0.8rem;
+    font-weight: 500;
+    color: #595959;
+}
+
+.desc {
+    font-size: 0.8rem;
+}
+</style>
