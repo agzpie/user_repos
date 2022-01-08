e69bf127f5f2b4f34528b28cdccb985ce4ad35c0
Agata Pietralik <agata.pietralik@student.wat.edu.pl>
2022-01-08 22:24:42 +0100

    Stars shine bright on hover

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="main">
     <h2 class="header1">Whose repositories would you like to see?</h2>
-    
-    <label for="user-name">github @: </label>
-    <input
-      v-model="name"
-      id="user-name"
-      name="user-name"
-      placeholder="github user name here"
-    />
-    <button class="accept-button" @click="userName = name; isActive = false">OK</button>
-    
 
-    <h4 :class="{inactive:isActive}">
+    <div class="container">
+      <div class="form-container">
+        <label for="user-name">github @: </label>
+        <input
+          v-model="name"
+          id="user-name"
+          name="user-name"
+          placeholder="github user name here"
+        />
+      </div>
+      <div class="btn-container">
+        <button
+          class="accept-button"
+          @click="
+            userName = name;
+            isActive = false;
+          "
+        >
+          OK
+        </button>
+      </div>
+    </div>
+
+    <h4 :class="{ inactive: isActive }">
       The most popular repositories of {{ userName }}:
     </h4>
 
-      <ListElement 
-        v-for="lib in orderedList" 
-        :key="lib.name" 
-        :repository-name="lib.name" 
-        :description="lib.description"
-        :stars="lib.stargazers_count">
-      </ListElement>
-
-     <!-- <li v-for="lib in data" :key="lib.name" >{{lib.name}}</li> -->
+    <ListElement
+      v-for="lib in orderedList"
+      :key="lib.name"
+      :repository-name="lib.name"
+      :description="lib.description"
+      :stars="lib.stargazers_count"
+    >
+    </ListElement>
   </div>
 </template>
 
 <script>
-import ListElement from './components/ListElement';
+import ListElement from "./components/ListElement";
 import { ref, reactive, toRefs, watchEffect, computed } from "vue";
 export default {
   name: "App",
@@ -41,25 +53,21 @@ export default {
   setup() {
     const name = ref(null);
     const userName = ref(null);
-    //const stargazers_count = ref(null);
     const state = reactive({ data: [] });
 
     watchEffect(() => {
-      if (userName.value){
+      if (userName.value) {
         fetch(`https://api.github.com/users/${userName.value}/repos`)
           .then((response) => response.json())
           .then((data) => {
             state.data = data;
             console.log("data", data);
             name.value = "";
-        });
+          })
+          .catch(err => console.log(err.message))
       }
     });
 
-    //const order = ref('state.data.stargazers_count');
-    //const handleClick = () => {
-    //  order.value = ref('state.data.stargazers_count');
-    //};
     const orderedList = computed(() => {
       return [...state.data].sort((a, b) => {
         return a.stargazers_count < b.stargazers_count ? 1 : -1;
@@ -72,7 +80,7 @@ export default {
       userName,
       ListElement,
       ...toRefs(state),
-      orderedList
+      orderedList,
     };
   },
 };
@@ -99,21 +107,30 @@ body {
   border-radius: 10px;
   transition: 3s;
   transition-timing-function: ease;
+  align-content: space-around;
 }
 
-.inactive {
-  color:rgb(226, 226, 226);
-  transition: 2s;
+.container {
+  display: flex;
 }
 
+.form-container {
+  align-self: flex-start;
+  flex-grow: 1;
+}
+.btn-container {
+  justify-content: center;
+  flex-grow: 1;
+}
 .accept-button {
+  align-self: center;
   color: white;
   font-weight: 600;
-  background: rgb(34,158,94);
-  background: radial-gradient(circle, #31a851 28%, rgba(31,149,52,1) 78%);
+  background: rgb(34, 158, 94);
+  background: radial-gradient(circle, #31a851 28%, rgba(31, 149, 52, 1) 78%);
   background-size: 100% auto;
-  margin: 0.5rem 2rem;
   padding: 0.25rem 1.5rem;
+  position: center;
   border: 1px solid #2c3e50;
   border-radius: 5px;
   transition: 0.3s all ease;
@@ -129,6 +146,20 @@ body {
   letter-spacing: 0.05rem;
 }
 
+.inactive {
+  color: rgb(226, 226, 226);
+  transition: 2s;
+}
 
+/* Extra small devices (phones, 450px and down) */
+@media only screen and (max-width: 450px) {
+  .container {
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 20vw;
+    align-items: center;
+  }
+}
 
 </style>
--- src/components/ListElement.vue
+++ src/components/ListElement.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <hr>
-        <p class="repo-name">
-            {{ repositoryName }}
+        <p class="repo-header">
+            <span class="repo-name">{{ repositoryName }}</span>
             &ensp;
-            <svg width="15" height="13" viewBox="0 -1 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.5 1L9.33389 4.97587L13.6819 5.49139L10.4673 8.46413L11.3206 12.7586L7.5 10.62L3.6794 12.7586L4.5327 8.46413L1.31813 5.49139L5.66611 4.97587L7.5 1Z" stroke="#666666" stroke-width="1.2" stroke-linejoin="round"/>
+            <svg class="icon" width="15" height="13" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg">
+                <path class="icon" d="M7.5 1L9.33389 4.97587L13.6819 5.49139L10.4673 8.46413L11.3206 12.7586L7.5 10.62L3.6794 12.7586L4.5327 8.46413L1.31813 5.49139L5.66611 4.97587L7.5 1Z" stroke-width="1.2" stroke-linejoin="round"/>
             </svg>
 
-            <span class="star"> {{ stars }}</span>
+            <span class="star">{{ stars }}</span>
         </p>
         <p class="desc">{{ description }}</p>
     </div>
@@ -34,19 +34,28 @@ hr {
     color: #0862CC;
     /* TODO make repository name a link */
 }
-
-.svg {
-    vertical-align: middle;;
+.repo-name:hover {
+    text-decoration: underline;
+    letter-spacing: 0.005rem;
 }
-
+.repo-header:hover .icon {
+    fill:rgb(245, 245, 21);
+    stroke:rgb(223, 223, 40);
+}
+.repo-header:hover .star {
+    color:rgb(223, 223, 40);
+}
+.icon {
+    fill: none;
+    vertical-align: middle;
+    stroke: #666666;
+}
 .star {
     vertical-align: middle;
     font-size: 0.8rem;
     font-weight: 500;
     color: #595959;
-    /* TODO make star golden on hover */
 }
-
 .desc {
     font-size: 0.8rem;
 }
